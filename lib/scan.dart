f4f5bda96f89ee21b77dd7715bbe63d7f77971c1
raenAnand <avis.anand@gmail.com>
2020-06-22 15:21:32 +0530

    Initial commit

---
+++ lib/scan.dart
@@ -0,0 +1,138 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class ScanScreen extends StatefulWidget {
+  @override
+  _ScanState createState() => new _ScanState();
+}
+
+class _ScanState extends State<ScanScreen> {
+  String barcode = "";
+  var data;
+  bool set=false;
+
+  @override
+  initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: new AppBar(
+          title: new Text('QR Code Scanner'),
+          backgroundColor: Colors.deepOrange,
+        ),
+        backgroundColor:
+            set?Color(int.parse(data['color_code'].replaceAll('#', '0xff'))): Colors.white,
+        body: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                child: RaisedButton(
+                    color: Colors.deepOrange,
+                    textColor: Colors.white,
+                    splashColor: Colors.blueGrey,
+                    onPressed: scan,
+                    child: const Text('START CAMERA SCAN')),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                child: Text(
+                  barcode,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+
+  Future scan() async {
+    try {
+      String code = await BarcodeScanner.scan();
+//      main(barcode);
+      setState(() {
+        set=true;
+        data = parseJwtPayLoad(code);
+      });
+      setState(() => this.barcode = data['message'].toString());
+    } on PlatformException catch (e) {
+      if (e.code == BarcodeScanner.CameraAccessDenied) {
+        setState(() {
+          this.barcode = 'The user did not grant the camera permission!';
+        });
+      } else {
+        setState(() => this.barcode = 'Unknown error: $e');
+      }
+    } on FormatException {
+      setState(() => this.barcode =
+          'null (User returned using the "back"-button before scanning anything. Result)');
+    } catch (e) {
+      setState(() => this.barcode = 'Unknown error: $e');
+    }
+  }
+
+  Map<String, dynamic> parseJwtPayLoad(String token) {
+    final parts = token.split('.');
+    if (parts.length != 3) {
+      throw Exception('invalid token');
+    }
+
+    final payload = _decodeBase64(parts[1]);
+    final payloadMap = json.decode(payload);
+    if (payloadMap is! Map<String, dynamic>) {
+      throw Exception('invalid payload');
+    }
+
+    return payloadMap;
+  }
+
+  Map<String, dynamic> parseJwtHeader(String token) {
+    final parts = token.split('.');
+    if (parts.length != 3) {
+      throw Exception('invalid token');
+    }
+
+    final payload = _decodeBase64(parts[0]);
+    final payloadMap = json.decode(payload);
+    if (payloadMap is! Map<String, dynamic>) {
+      throw Exception('invalid payload');
+    }
+
+    return payloadMap;
+  }
+
+  String _decodeBase64(String str) {
+    String output = str.replaceAll('-', '+').replaceAll('_', '/');
+
+    switch (output.length % 4) {
+      case 0:
+        break;
+      case 2:
+        output += '==';
+        break;
+      case 3:
+        output += '=';
+        break;
+      default:
+        throw Exception('Illegal base64url string!"');
+    }
+
+    return utf8.decode(base64Url.decode(output));
+  }
+
+//String
+
+  void main(b) {
+//    String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+    print(parseJwtHeader(b));
+    print(parseJwtPayLoad(b));
+  }
+}
